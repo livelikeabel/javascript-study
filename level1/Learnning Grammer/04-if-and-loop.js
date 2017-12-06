89156a3c4987404fbd3793659f7e9907331ab9ab
abelko <esung1129@gmail.com>
2017-12-06 13:08:00 +0900

    Learnning Grammer/loop-tutorial.js,html

---
+++ level1/Learnning Grammer/04-if-and-loop.js
@@ -0,0 +1,35 @@
+//break
+// console.log("start");
+
+// for (var i = 0; i < 5; i++) {
+// 	console.log("1 %d", i);
+// 	if(i == 3) {
+// 		break;
+// 	}
+// 	console.log("2 %d". i);
+// }
+
+// console.log("end");
+
+//continue
+// console.log("start");
+
+// for (var i = 0; i < 5; i++) {
+// 	console.log("1 %d", i);
+// 	if (i >= 3) {
+// 		continue;
+// 	}
+// 	console.log("2 %d", i);
+// }
+
+// console.log("end");
+
+// var i = 1
+// var hap = 0;
+// while (i <= 100) {
+// 	if (i % 2 !== 0) {
+// 		hap += i;
+// 	}
+//  ++i
+// }
+// console.log(hap);
---
+++ level1/Learnning Grammer/05-loop-tutorial.js
@@ -0,0 +1,25 @@
+// 해결 해야 할 것
+// 사용자가 입력한 숫자들을 볼 수 있는 기록 기능
+// 메소드로 쪼개
+
+console.log("숫자 맞추기 게임");
+
+var randomNumber  = Math.floor(Math.random()*100+1); //1~100까지의 랜덤수가 나오긴 함 ㅇㅇ 근데 좋은 방법일까?
+console.log(randomNumber);
+
+var count = 0;
+while (true) {
+	var userNumber = prompt("1~100 까지의 숫자 1개를 입력해 주세요");
+	//console.log(userNumber);
+	count += 1;
+	console.log("입력한 숫자 : " + userNumber);
+	document.write("입력한 숫자 : " + userNumber + "</br>");
+
+	if (randomNumber == userNumber) {
+		alert("숫자를 맞추었습니다!");
+		document.write( count + "번 시도 하였습니다.");
+		break;
+	} else {
+		alert("다시 시도하세요");
+	}	
+}
