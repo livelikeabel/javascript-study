e7561524e5e34f787db077cba30e6fb25db09b6e
abelko <esung1129@gmail.com>
2017-12-13 22:00:59 +0900

    js-for-kids/canvas 경로 색칠하기

---
+++ js-for-kids/canvas/canvas2.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+
+ctx.fillStyle = "SkyBlue";
+ctx.beginPath();
+ctx.moveTo(100, 100);
+cix.lineTo(100, 60);
+cix.lineTo(130, 30);
+cix.lineTo(160, 60);
+cix.lineTo(160, 100);
+cix.lineTo(100, 100);
+ctx.fill();
