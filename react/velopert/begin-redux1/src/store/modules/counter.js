82585e0ce5b936bfd84fb5953264dadf917b352c
Abel ko <esung1129@gmail.com>
2018-05-13 19:56:48 +0900

    counter 모듈 작성하기

--- react/velopert/begin-redux1/src/store/modules/counter.js
+++ react/velopert/begin-redux1/src/store/modules/counter.js
@@ -1,1 +1,27 @@
 // 카운터 관련 상태 로직
+
+// 액션 타입을 정의해줍니다.
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+
+// 액션 생성 함수를 만듭니다.
+// 이 함수들은 나중에 다른 파일에서 불러와야 하므로 내보내줍니다.
+export const increment = () => ({ type: INCREMENT });
+export const decrement = () => ({ type: DECREMENT });
+
+// 모듈의 초기 상태를 정의합니다.
+const initialState = {
+    number: 0
+};
+
+//리듀서를 만들어서 내보내줍니다.
+export default function reducer(state = initailState, action) {
+    switch(action.type) {
+        case INCREMENT:
+            return { number: state.number + 1 };
+        case DECREMENT:
+            return { number: state.number - 1 };
+        default:
+            return state; // 아무일도 없으면, 현재 상태를 그대로 반환한다.
+    }
+}
