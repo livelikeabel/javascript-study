c7925daa234f5ea91141b58973a38e55bbaaa367
Abel ko <esung1129@gmail.com>
2018-05-17 11:04:05 +0900

    route와 파라미터

--- react/velopert/react-router1/src/shared/App.js
+++ react/velopert/react-router1/src/shared/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home, About } from "pages";
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                hello react-router
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <header>이건 헤더야</header>
+        <Route exact path="/" component={Home} />
+        <Switch>
+          <Route path="/about/:name" component={About} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
