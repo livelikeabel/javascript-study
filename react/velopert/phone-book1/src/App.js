bbbc26a9d8987552b8f767124300cca682f51ff4
Abel ko <esung1129@gmail.com>
2018-06-05 18:40:59 +0900

    데이터 추가

--- react/velopert/phone-book1/src/App.js
+++ react/velopert/phone-book1/src/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data})
+    })
   }
   render() {
+    const { information } = this.state;
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
+        {JSON.stringify(information)}
       </div>
     );
   }
