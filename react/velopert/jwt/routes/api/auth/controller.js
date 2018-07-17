1d7829d40ecc6fbcb29450f595ded4bbdd63d686
livelikeabel <esung1129@gmail.com>
2018-07-17 10:52:35 +0900

    로그인 구현하기

--- react/velopert/jwt/routes/api/auth/controller.js
+++ react/velopert/jwt/routes/api/auth/controller.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const User = require('../../../models/user')
 
 /*
@@ -12,8 +13,8 @@ exports.register = (req, res) => {
   let newUser = null
 
   //create a new user if does not exist
-  const create = (user) => {
-    if(user) {
+  const create = user => {
+    if (user) {
       throw new Error('username exists')
     } else {
       return User.create(username, password)
@@ -21,14 +22,14 @@ exports.register = (req, res) => {
   }
 
   // count the number of the user
-  const count = (user) => {
+  const count = user => {
     newUser = user
     return User.count({}).exec()
   }
 
   // assign admin if count is 1
-  const assign = (count) => {
-    if(count === 1) {
+  const assign = count => {
+    if (count === 1) {
       return newUser.assignAdmin()
     } else {
       // if not, return a promise that returns false
@@ -37,7 +38,7 @@ exports.register = (req, res) => {
   }
 
   // respond to the client
-  const respond = (isAdmin) => {
+  const respond = isAdmin => {
     res.json({
       message: 'registered successfully',
       admin: isAdmin ? true : false
@@ -45,7 +46,7 @@ exports.register = (req, res) => {
   }
 
   // run when there is an error (username exists)
-  const onError = (error) => {
+  const onError = error => {
     res.status(409).json({
       message: error.message
     })
@@ -53,9 +54,94 @@ exports.register = (req, res) => {
 
   //check username duplication
   User.findOneByUsername(username)
-  .then(create)
-  .then(count)
-  .then(assign)
+    .then(create)
+    .then(count)
+    .then(assign)
+    .then(respond)
+    .catch(onError)
+}
+
+/*
+    POST /api/auth/login
+    {
+        username,
+        password
+    }
+*/
+
+exports.login = (req, res) => {
+  const { username, password } = req.body
+  const secret = req.app.get('jwt-secret')
+
+  // check the user info & generate the jwt
+  const check = user => {
+    if (!user) {
+      // user does not exist
+      throw new Error('login failed')
+    } else {
+      // user exists, check the password
+      if (user.verify(password)) {
+        // create a promise that generates jwt asynchronously
+        const p = new Promise((resolve, reject) => {
+          jwt.sign(
+            {
+              _id: user._id,
+              username: user.username,
+              admin: user.admin
+            },
+            secret,
+            {
+              expiresIn: '7d',
+              issuer: 'abel.com',
+              subject: 'userInfo'
+            }, (err, token) => {
+                if (err) reject(err)
+                resolve(token)
+            })
+        })
+          return p
+      } else {
+          throw new Error('login faild')
+      }
+    }
+  }
+
+  // error occured
+  const onError = (error) => {
+    res.status(403).json({
+        message: error.message
+    })
+  }
+
+  // find the user
+  User.findOneByUsername(username)
+  .then(check)
   .then(respond)
   .catch(onError)
 }
+
+/*
+
+사용법: jwt.sign(payload, secret, options, [callback])
+
+만약에 callback 이 전달되면 비동기적으로 작동하며, 콜백함수의 파라미터는 (err, token) 입니다. 전달되지 않을시엔 동기적으로 작동하며, JWT 를 문자열 형태로 리턴합니다.
+
+payload 는  객체, buffer, 혹은 문자열형태로 전달 될 수있습니다.
+
+secret 은 서명을 만들 때 사용되는 알고리즘에서 사용되는 문자열 혹은 buffer 형태의 값 입니다.
+
+options:
+
+algorithm: 기본값은 HS256 으로 지정됩니다.
+expiresIn: JWT 의 등록된 클레임중 exp 값을 x 초후 혹은 rauchg/ms 형태의 기간 후로 설정합니다.
+(예제: (60, “2 days”, “10h”, “7d”)
+notbefore: JWT 의 등록된 클레임중 nbf 값을 x 초후 혹은 rauchg/ms 형태의 기간 후로 설정합니다.
+(예제: (60, “2 days”, “10h”, “7d”)
+audience
+issuer
+jwtid
+subject
+noTimestamp
+header
+
+*/
--- react/velopert/jwt/routes/api/auth/index.js
+++ react/velopert/jwt/routes/api/auth/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const controller = require('./controller')
 
 router.post('/register', controller.register)
+router.post('/login', controller.login)
 
 module.exports = router
