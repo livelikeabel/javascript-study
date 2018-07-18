8822374cf709543e8f7726027a976d9d58d199b8
livelikeabel <esung1129@gmail.com>
2018-07-18 10:29:07 +0900

    JWT 검증 구현

--- react/velopert/jwt/routes/api/auth/controller.js
+++ react/velopert/jwt/routes/api/auth/controller.js
@@ -106,6 +106,14 @@ exports.login = (req, res) => {
     }
   }
 
+  // respond the token
+  const respond = (token) => {
+    res.json({
+      message: 'logged in successfully',
+      token
+    })
+  }
+
   // error occured
   const onError = (error) => {
     res.status(403).json({
@@ -145,3 +153,49 @@ noTimestamp
 header
 
 */
+
+/*
+    GET /api/auth/check
+*/
+
+exports.check = (req, res) => {
+  // read the token from header or url
+  const token = req.headers['x-access-token'] || req.query.token
+
+  // token does not exist
+  if(!token) {
+    return res.status(403).json({
+      success: false,
+      message: 'not logged in'
+    })
+  }
+
+  // create a promise that decodes the token
+  const p = new Promise(
+    (resolve, reject) => {
+      jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
+        if(err) reject(err)
+        resolve(decoded)
+      })
+    }
+  )
+
+  // if token is valid, it will respond with its info
+  const respond = (token) => {
+    res.json({
+      success: true,
+      info: token
+    })
+  }
+
+  // if it has failed to verify, it will return an error message
+  const onError = (error) => {
+    res.status(403).json({
+        success: false,
+        message: error.message
+    })
+  }
+
+  // process the promise
+  p.then(respond).catch(onError)
+}
--- react/velopert/jwt/routes/api/auth/index.js
+++ react/velopert/jwt/routes/api/auth/index.js
@@ -3,5 +3,6 @@ const controller = require('./controller')
 
 router.post('/register', controller.register)
 router.post('/login', controller.login)
+router.get('/check', controller.check)
 
 module.exports = router
