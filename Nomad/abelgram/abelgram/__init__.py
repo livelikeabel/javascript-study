c4085cc25ae773f079d1d3b1a5ee55120428f0cc
livelikeabel <esung1129@gmail.com>
2018-07-27 20:14:30 +0900

    init project

---
+++ Nomad/abelgram/abelgram/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "Cloning IG with Py"
+__version_info__ = tuple(
+    [
+        int(num) if num.isdigit() else num
+        for num in __version__.replace("-", ".", 1).split(".")
+    ]
+)
