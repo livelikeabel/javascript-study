b7a83bced8ea22a4e9bca4c2693a631b2445eba9
livelikeabel <esung1129@gmail.com>
2018-08-21 13:25:26 +0900

    webpack study

--- Nomad/abelgram/abelgram/users/models.py
+++ Nomad/abelgram/abelgram/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -9,6 +10,7 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    website = models.URLFiled()
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
