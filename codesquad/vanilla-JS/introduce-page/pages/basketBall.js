e97260d193b136cdb7ef7eade7ee5f18970fa4fc
livelikeabel <esung1129@gmail.com>
2018-10-05 10:26:51 +0900

    study querySelector and querySelectorALl

---
+++ codesquad/vanilla-JS/introduce-page/pages/basketBall.js
@@ -0,0 +1,8 @@
+let jordanImg = document.getElementById('jordanImage')
+// jordanImg.addEventListener.onclick('hi im jordan!')
+let title = document.querySelector('h1')
+// title.innerHTML('<p>좋아하는 순서대로</p>')
+let firstListAtNav = document.querySelector('li')
+let liList = document.querySelectorAll('li')
+// document.querySelector('.myTitle')
+let loginInput = document.querySelector("div.user-panel.main input[name=login]")
