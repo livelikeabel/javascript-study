f0c4f89347395f13d21ed2426e737d95fefa3d05
livelikeabel <esung1129@gmail.com>
2018-12-11 17:42:25 +0900

    Add Header component

--- react/velopert/blog/blog-frontend/src/components/common/Header/Header.js
+++ react/velopert/blog/blog-frontend/src/components/common/Header/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import styles from './Header.scss'
 import classnames from 'classnames/bind'
+import { Link } from 'react-router-dom'
 
 const cx = classnames.bind(styles)
 
 const Header = () => (
-  <div className={cx('header')}>
-    Header
-  </div>
+  <header className={cx('header')}>
+    <div className={cx('header-content')}>
+      <div className={cx('brand')}>
+        <Link to="/">Abel blog</Link>
+      </div>
+      <div className={cx('right')}>
+        {/* 조건에 따라 버튼 렌더링 */}
+        오른쪽
+      </div>
+    </div>
+  </header>
 )
 
 export default Header
--- react/velopert/blog/blog-frontend/src/components/common/PageTemplate/PageTemplate.js
+++ react/velopert/blog/blog-frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './PageTemplate.scss'
 import classnames from 'classnames/bind'
+import Header from 'components/common/Header'
 
 const cx = classnames.bind(styles)
 
 const PageTemplate = () => (
   <div className={cx('page-template')}>
-    PageTemplate
+    <Header />
   </div>
 )
 
