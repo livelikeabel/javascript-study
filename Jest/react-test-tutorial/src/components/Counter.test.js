213aa62f404984218405902ae1d20504e303aa4b
abelko <esung1129@gmail.com>
2019-01-21 20:03:05 +0900

    내부 메소드 호출 및 state조회

--- Jest/react-test-tutorial/src/components/Counter.test.js
+++ Jest/react-test-tutorial/src/components/Counter.test.js
@@ -13,4 +13,20 @@ describe('Counter', () => {
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
     })
+    
+    // increase가 잘 되는지 확인
+    it('increases correctly', () => {
+        component.getInstance().onIncrease();
+        expect(component.getInstance().state.value).toBe(2); //value의 값이 2인지 확인
+        const tree = component.toJSON(); // re-render
+        expect(tree).toMatchSnapshot(); // 스냅샷 비교
+    })
+    
+    //decrease가 잘 되는지 확인
+    it('decreases correctly', () => {
+        component.getInstance().onDecrease();
+        expect(component.getInstance().state.value).toBe(1);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    })
 })
