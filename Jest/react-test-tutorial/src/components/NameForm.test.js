2f361ab584fb84c285035436fd5ff28e11337bfc
abelko <esung1129@gmail.com>
2019-01-22 13:22:54 +0900

    enzyme init

--- Jest/react-test-tutorial/src/components/NameForm.test.js
+++ Jest/react-test-tutorial/src/components/NameForm.test.js
@@ -1,16 +1,29 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+// import renderer from 'react-test-renderer'
+import {shallow} from 'enzyme'
 import NameForm from './NameForm'
+
+// describe('NameForm', () => {
+//     let component = null;
+
+//     it('renders correctly', () => {
+//         component = renderer.create(<NameForm/>)
+//     })
+
+//     it('matches snapshot', () => {
+//         const tree = component.toJSON();
+//         expect(tree).toMatchSnapshot();
+//     })
+// })
 
 describe('NameForm', () => {
     let component = null;
-    
+
     it('renders correctly', () => {
-        component = renderer.create(<NameForm/>)
+        component = shallow(<NameForm />)
     })
 
     it('matches snapshot', () => {
-        const tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
+        expect(component).toMatchSnapshot();
     })
 })
