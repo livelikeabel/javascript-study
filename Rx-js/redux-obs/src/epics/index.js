24048d4d907cfc6838b902699b006e49fd17e567
abelko <esung1129@gmail.com>
2019-02-26 11:15:52 +0900

    Use redux-observable for simple Ajax requests

--- Rx-js/redux-obs/src/epics/index.js
+++ Rx-js/redux-obs/src/epics/index.js
@@ -1,19 +1,24 @@
 import {Observable} from 'rxjs';
 import {combineEpics} from 'redux-observable';
-import {LOAD_STORIES, clear} from '../actions';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/ignoreElements';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/switchMap';
+import "rxjs/add/observable/dom/ajax";
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/observable/of';
+import {FETCH_USER, fetchUserFulfilledAction} from '../actions';
 
-function loadStoriesEpic(action$) {
+function fetchUserEpic(action$) {
   return action$
-    .ofType(LOAD_STORIES)
-    .switchMap(() => {
-      return Observable.of(clear()).delay(2000);
+    .ofType(FETCH_USER)
+    .switchMap(({payload}) => {
+      return Observable.ajax.getJSON(`https://api.github.com/users/${payload}`)
+        .map(user => {
+          return fetchUserFulfilledAction(user)
+        })
     })
 }
 
-export const rootEpic = combineEpics(loadStoriesEpic);
+export const rootEpic = combineEpics(fetchUserEpic);
