7ada0c49845fb9665e53a30f984a9cfc5d30580c
abelko <esung1129@gmail.com>
2019-03-04 10:11:23 +0900

    use unsubscribe in Observable

---
+++ Rx-js/rxjs-programming/2-5.js
@@ -0,0 +1,19 @@
+const {Observable} = require('rxjs');
+
+const observableCreated$ = Observable.create(function subscribe(observer) {
+  // intervalID 자원 추적
+  const intervalID = setInterval(function() {
+    observer.next('hi');
+  }, 1000);
+  
+  // intervalID 자원을 해제하고 재배치하는 방법을 제공
+  return function unsubscribe() {
+    clearInterval(intervalID);
+  };
+});
+
+observableCreated$.subscribe(
+  function next(item) { console.log(item)},
+  function error(e) {},
+  function complete() { console.log('complete')}
+)
