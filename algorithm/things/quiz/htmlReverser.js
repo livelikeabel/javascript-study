4c70d07e2a96b9de4490e392f9b4533259641422
Abel <esung1129@gmail.com>
2019-05-13 20:47:19 +0900

    :sparkles:htmlReverser 첫번째 테스트 통과

--- algorithm/things/quiz/htmlReverser.js
+++ algorithm/things/quiz/htmlReverser.js
@@ -1,5 +1,33 @@
 const htmlReverser = htmlText => {
-  return 'hi'
+  let flag = '';
+  let innerText = '';
+  const reversedHtmlText = htmlText.split("").reduce((acc, cur, i, arr) => {
+    if (cur === '<') {
+      if (flag === 'innerText' && arr[i + 1] === '/') {
+        flag = '</';
+        return acc + innerText.split("").reverse().join("") + cur;
+      }
+      flag = '<'
+      return acc + cur;
+    }
+    if (flag === '<') {
+      if (cur === '>') {
+        flag = 'innerText'
+      }
+      return acc + cur;
+    }
+    if (flag === 'innerText') {
+      innerText += cur
+      return acc
+    }
+    if (flag === '</') {
+      return acc + cur
+    }
+  }, '');
+
+  return reversedHtmlText;
 }
+
+console.log(htmlReverser('<span>abel</span>'))
 
 module.exports = htmlReverser;
