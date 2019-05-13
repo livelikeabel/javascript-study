efdb1f2a4d874910bb2bce7fbd65184caf7622cf
Abel <esung1129@gmail.com>
2019-05-13 20:04:20 +0900

    :contruction:파일위치 변경

---
+++ algorithm/things/sampleTest.js
@@ -0,0 +1,26 @@
+// function sum(a,b) {
+//   return a + b;
+// }
+
+
+// function test(title, testCode) {
+//   try {
+//     testCode();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// function expect(result) {
+//   return {
+//     toBe(expected) {
+//       if (result !== expected) {
+//         throw new Error(result + ' is not equal to ' + expected);
+//       }
+//     }
+//   }
+// }
+
+// test('sum(1, 2) is not equal 5', function() {
+//   expect(sum(1,2)).toBe(5);
+// });
