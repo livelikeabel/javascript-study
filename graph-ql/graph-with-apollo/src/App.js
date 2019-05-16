aebbde9cfcdc76b24ca2011510e50d12b5f10a17
Abel <esung1129@gmail.com>
2019-05-16 09:12:08 +0900

    :art:fetch data using apollo query component 2/2

--- graph-ql/graph-with-apollo/src/App.js
+++ graph-ql/graph-with-apollo/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
-import gql from 'graphql-tag';
-import { ApolloProvider, Query } from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
+import Recipes from './Recipes';
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/"
@@ -10,29 +10,8 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div>Hello</div>
-      <Query
-        query={gql`
-          {
-            recipes {
-              id
-              title
-            }
-          }
-        `}
-      >
-        {({ data }) => {
-          if (data.recipes === undefined) return null;
-
-          return (
-            <ul>
-              {data.recipes.map(({ id, title }) =>
-                <li key={id}>{title}</li>
-              )}
-            </ul>
-          )
-        }}
-      </Query>
+      Hello
+     <Recipes/>
     </ApolloProvider>
   )
 }
