cb236aeb47ab8eb514f66f101d0336fd251fd077
Abel <esung1129@gmail.com>
2019-05-16 16:27:59 +0900

    :art:Manage Local state using Apollo by extending the GraphQL Schema on the Client

--- graph-ql/graph-with-apollo/src/Recipes.jsx
+++ graph-ql/graph-with-apollo/src/Recipes.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import recipesQuery from './recipesQuery';
+import gql from 'graphql-tag';
+
+const updateRecipeStarredMutation = gql`
+  mutation updateRecipeStarred($id: ID!, $isStarred: Boolean!) {
+    updateRecipeStarred(id: $id, isStarred: $isStarred) @client
+  }
+`
 
 export default function Recipes() {
 
@@ -27,8 +34,45 @@ export default function Recipes() {
 
           return (
             <ul>
-              {data.recipes.map(({ id, title }) =>
-                <li key={id}>{title}</li>
+              {data.recipes.map(({ id, title, isStarred }) =>
+                <li key={id}>
+                  {title}
+                  <Mutation
+                    mutation={updateRecipeStarredMutation}
+                    refetchQueries={[
+                      {
+                        query: recipesQuery,
+                        variables: { vegetarian: false }
+                      },
+                      {
+                        query: recipesQuery,
+                        variables: { vegetarian: true }
+                      }
+                    ]}
+                    awaitRefetchQueries={true}
+                  >
+                    {(updateRecipeStarred, { loading, error }) => (
+                      <button
+                        onClick={() =>
+                          updateRecipeStarred({
+                            variables: {
+                              id,
+                              isStarred: !isStarred
+                            }
+                          })
+                        }
+                        className="star-btn"
+                        style={{
+                          color: isStarred ? "orange" : "grey",
+                          animation: loading ?
+                            "inflate 0.7 ease infinite alternate" :
+                            "none"
+                        }}>
+                        ⭑ {error && "Failed to update"}
+                      </button>
+                    )}
+                  </Mutation>
+                </li>
               )}
             </ul>
           )
