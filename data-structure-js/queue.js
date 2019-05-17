6d92d0fb5c54f02328eac4f00356a6906a826c59
Abel <esung1129@gmail.com>
2019-05-17 20:04:43 +0900

    :sparkles:Queue Data structure in js

---
+++ data-structure-js/queue.js
@@ -0,0 +1,35 @@
+// Queues
+// first-in first-out : FIFO
+
+function createQueue() {
+  const queue = [] // closer
+
+  return {
+    enqueue(item) {
+      queue.unshift(item)
+    },
+    dequeue() {
+      return queue.pop()
+    },
+    peek() {
+      return queue[queue.length - 1]
+    },
+    get length() {
+      return queue.length
+    },
+    isEmpty() {
+      return queue.length === 0
+    }
+  }
+}
+
+
+const q = createQueue()
+console.log(q.isEmpty())
+
+q.enqueue('Make an egghead lesson')
+q.enqueue('Help others learn')
+q.enqueue('Be happy')
+
+q.dequeue()
+console.log(q.peek())
