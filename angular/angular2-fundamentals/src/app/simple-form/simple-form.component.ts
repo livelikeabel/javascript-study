63c00fed94a3ba13d6681656ff04faf8871c25f9
Abel <esung1129@gmail.com>
2019-06-04 22:32:20 +0900

    :art:manage angular2 elements with events and refs

--- angular/angular2-fundamentals/src/app/simple-form/simple-form.component.ts
+++ angular/angular2-fundamentals/src/app/simple-form/simple-form.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-simple-form',
+  selector: "app-simple-form",
   template: `
-    <p>
-      simple-form works!
-    </p>
+    <div>
+      <input #myInput type="text" />
+      <button (click)="onClick(myInput.value)">Click me!</button>
+    </div>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  onClick(value) {
+    console.log(value);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
