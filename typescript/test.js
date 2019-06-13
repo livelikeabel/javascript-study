e3c4afdf06daac8809735740ab81a08b0157f7a0
Abel <esung1129@gmail.com>
2019-06-13 23:33:03 +0900

    :art:ts study

--- typescript/test.js
+++ typescript/test.js
@@ -1,6 +1,31 @@
 "use strict";
-var AppComponent = /** @class */ (function () {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Parent = /** @class */ (function () {
+    function Parent() {
+        this.one = 1; // 외부접근가능
+        this.two = 2;
+        this.three = 3;
+    }
+    return Parent;
+}());
+var AppComponent = /** @class */ (function (_super) {
+    __extends(AppComponent, _super);
     function AppComponent() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
     return AppComponent;
-}());
+}(Parent));
+var kim;
+var p = new Parent();
--- typescript/test.ts
+++ typescript/test.ts
@@ -1,1 +1,53 @@
-class AppComponent {}
+class Parent {
+  public one = 1; // 외부접근가능
+  protected two = 2; // Parent를 상속한 객체에서만 접근할 수 있음
+  private three = 3;
+
+  constructor() {
+    this.one;
+    this.two;
+    this.three;
+  }
+}
+
+class AppComponent extends Parent {
+  constructor() {
+    super();
+    this.one;
+    this.two;
+  }
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+let kim: Person;
+
+let p = new Parent();
+new AppComponent();
+
+class Car {
+  constructor(public age: number) {}
+}
+
+let car = new Car(10);
+
+car.age;
+
+function Component(constructorFn?: Function) {
+  /**
+   * 클래스를 꾸며주는 로직
+   * Component, Html, Css
+   */
+
+  return constructorFn;
+}
+
+@Component()
+class DecoComponent {
+  @input() height: number;
+
+  constructor() {}
+}
