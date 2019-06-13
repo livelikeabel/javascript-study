6f95e430dedf1b9789b2fcd088be01c1c485868c
Abel <esung1129@gmail.com>
2019-06-14 07:47:08 +0900

    :art: 컴포넌트 배치

--- angular/hello-angular/front-end/src/app/app.module.ts
+++ angular/hello-angular/front-end/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SectionComponent } from './section/section.component';
+import { TimeDisplayComponent } from './section/time-display/time-display.component';
+import { ButtonsComponent } from './section/buttons/buttons.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       HeaderComponent,
       FooterComponent,
-      SectionComponent
+      SectionComponent,
+      TimeDisplayComponent,
+      ButtonsComponent
    ],
    imports: [
       BrowserModule,
---
+++ angular/hello-angular/front-end/src/app/section/time-display/time-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-display',
+  templateUrl: './time-display.component.html',
+  styleUrls: ['./time-display.component.scss']
+})
+export class TimeDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
