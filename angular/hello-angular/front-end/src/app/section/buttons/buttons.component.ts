4f9145cafd1b8883089b2a9f619e0fdd32d52ebf
Abel <esung1129@gmail.com>
2019-06-14 08:17:12 +0900

    :art: data binding 1

--- angular/hello-angular/front-end/src/app/section/buttons/buttons.component.ts
+++ angular/hello-angular/front-end/src/app/section/buttons/buttons.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-buttons',
-  templateUrl: './buttons.component.html',
-  styleUrls: ['./buttons.component.scss']
+  selector: "app-buttons",
+  templateUrl: "./buttons.component.html",
+  styleUrls: ["./buttons.component.scss"]
 })
 export class ButtonsComponent implements OnInit {
+  count: number = 0;
+  clear;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  onStartCount($event: MouseEvent) {
+    this.count++;
+    this.clear = setInterval(() => this.count++, 1000);
   }
 
+  onStopCount($event: MouseEvent) {
+    clearInterval(this.clear);
+  }
+
+  ngOnInit() {}
 }
--- angular/hello-angular/front-end/src/app/section/time-display/time-display.component.ts
+++ angular/hello-angular/front-end/src/app/section/time-display/time-display.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-time-display',
-  templateUrl: './time-display.component.html',
-  styleUrls: ['./time-display.component.scss']
+  selector: "app-time-display",
+  templateUrl: "./time-display.component.html",
+  styleUrls: ["./time-display.component.scss"]
 })
 export class TimeDisplayComponent implements OnInit {
+  time = 1;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setInterval(() => {
+      this.time++;
+    }, 1000);
   }
 
+  ngOnInit() {}
 }
