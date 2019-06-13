86512b35469002968f993042aedb6be18e061b7c
Abel <esung1129@gmail.com>
2019-06-13 23:49:46 +0900

    :sparkles: header, section, footer component 추가

--- angular/hello-angular/front-end/src/app/app.module.ts
+++ angular/hello-angular/front-end/src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello/hello.component";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
-  declarations: [AppComponent, HelloComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      SectionComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
--- angular/hello-angular/front-end/src/app/hello/hello.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "hello",
-  templateUrl: "./hello.component.html",
-  styleUrls: ["./hello.component.css"]
-})
-export class HelloComponent {}
