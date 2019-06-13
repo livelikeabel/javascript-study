4f44c446c700286d9cce773dd40d9bb5bd15abd6
Abel <esung1129@gmail.com>
2019-06-13 13:05:18 +0900

    :art:hello component 만들기

---
+++ angular/hello-angular/front-end/src/app/hello/hello.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "hello",
+  templateUrl: "./hello.component.html",
+  styleUrls: ["./hello.component.css"]
+})
+export class HelloComponent {}
