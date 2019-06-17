2cccd53809eeb2c0e2097ccbfeec0e1037b94845
Abel <esung1129@gmail.com>
2019-06-17 21:08:10 +0900

    :art: 라우터 모듈 추가

--- angular/hello-angular/front-end/src/app/section/section.module.ts
+++ angular/hello-angular/front-end/src/app/section/section.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SectionComponent } from "./section.component";
 import { StopwatchModule } from "./stopwatch/stopwatch.module";
+import { RouterModule, Routes } from "@angular/router";
+import { StopwatchComponent } from "./stopwatch/stopwatch.component";
+import { ClockComponent } from "./clock/clock.component";
+
+const routes: Routes = [
+  {
+    path: "stopwatch",
+    component: StopwatchComponent
+  },
+  {
+    path: "clock",
+    component: ClockComponent
+  }
+];
 
 @NgModule({
-  declarations: [SectionComponent],
-  exports: [SectionComponent],
-  imports: [CommonModule, StopwatchModule]
+  declarations: [SectionComponent, ClockComponent],
+  exports: [SectionComponent, RouterModule],
+  imports: [CommonModule, StopwatchModule, RouterModule.forChild(routes)]
 })
 export class SectionModule {}
--- angular/hello-angular/front-end/src/app/section/stopwatch/stopwatch.module.ts
+++ angular/hello-angular/front-end/src/app/section/stopwatch/stopwatch.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { TimeDisplayComponent } from "./time-display/time-display.component";
 import { ButtonsComponent } from "./buttons/buttons.component";
 import { StopwatchComponent } from "./stopwatch.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [TimeDisplayComponent, ButtonsComponent, StopwatchComponent],
   exports: [StopwatchComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule]
 })
 export class StopwatchModule {}
