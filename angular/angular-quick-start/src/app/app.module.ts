9ab6200a7114b4dc73e0d34b4ee4cdeb8d07a8b4
Abel <esung1129@gmail.com>
2019-06-19 06:09:49 +0900

    :art: angular component study1

---
+++ angular/angular-quick-start/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TestComponent } from "./test.component";
+
+@NgModule({
+  declarations: [AppComponent, TestComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ angular/angular-quick-start/src/app/test.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-test",
+  template: `
+    <p>오늘을 첫 시간</p>
+  `
+})
+export class TestComponent {}
