38bfa7c9ddc4e6ff9a63edd215218e6f4b165c06
abelko <esung1129@gmail.com>
2019-06-25 18:57:19 +0900

    :art: angular httpmodule study

---
+++ angular/angular-semina/angular-service-basic4/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-service-basic4';
+  users = [];
+  constructor(
+    @Inject('apiUrl') private apiUrl: string,
+    private http: HttpClient
+  ) {
+    this.http.get(this.apiUrl)
+      .toPromise()
+      .then(data => {
+        console.log(data)
+        this.users = data;
+      })
+      .catch(error => {
+        this.users = [];
+        console.log(error)
+      })
+  }
+}
---
+++ angular/angular-semina/angular-service-basic4/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [
+    {
+      provide: 'apiUrl',
+      useValue: 'https://api.github.com/users'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
