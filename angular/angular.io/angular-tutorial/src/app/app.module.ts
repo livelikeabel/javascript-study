0f078177b3ae968df803d81fdfd62b99c466e77e
abelko <esung1129@gmail.com>
2019-07-18 22:54:28 +0900

    :tada: init angular project

---
+++ angular/angular.io/angular-tutorial/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
