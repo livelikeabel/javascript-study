212384a0180c830192818b429a1610316fdc253b
abelko <esung1129@gmail.com>
2019-07-18 23:07:54 +0900

    :art: add top-bar component

--- angular/angular.io/angular-tutorial/src/app/product-list/product-list.component.ts
+++ angular/angular.io/angular-tutorial/src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  products = products;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  share() {
+    window.alert('The product has been shared!');
   }
 
 }
