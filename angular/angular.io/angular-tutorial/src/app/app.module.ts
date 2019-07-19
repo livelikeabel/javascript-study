578a9da64abd3dfef1f1f6e6574e132a81b385e0
abelko <esung1129@gmail.com>
2019-07-19 22:26:28 +0900

    :art: add @input

--- angular/angular.io/angular-tutorial/src/app/app.module.ts
+++ angular/angular.io/angular-tutorial/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    TopBarComponent
+    TopBarComponent,
+    ProductAlertsComponent
   ],
   imports: [
     BrowserModule,
