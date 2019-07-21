beb42081be6ba6c4c9eaf945d0cf351eb29f5bb4
abelko <esung1129@gmail.com>
2019-07-21 23:26:02 +0900

    :art: @Output

--- angular/angular.io/angular-tutorial/src/app/product-list/product-list.component.ts
+++ angular/angular.io/angular-tutorial/src/app/product-list/product-list.component.ts
@@ -9,8 +9,6 @@ import { products } from '../products';
 export class ProductListComponent implements OnInit {
   products = products;
 
-  constructor() { }
-
   ngOnInit() {
   }
 
@@ -18,4 +16,7 @@ export class ProductListComponent implements OnInit {
     window.alert('The product has been shared!');
   }
 
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
