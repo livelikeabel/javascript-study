97764047eb2b803bb75e0a0676db2b091fa92e31
abelko <esung1129@gmail.com>
2019-07-31 10:16:40 +0900

    :art: study rgrx action

--- ngrx/ngrx-tutorial/src/app/counter.actions.ts
+++ ngrx/ngrx-tutorial/src/app/counter.actions.ts
@@ -1,6 +1,11 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 
 export const increment = createAction('[Counter Component] Increment');
 export const decrement = createAction('[Counter Component] Decrement');
 export const reset = createAction('[Counter Component] Reset');
 
+// `createAction` function returns a function
+export const login = createAction(
+    '[Login Page] Login',
+    props<{ username: string, password: string }>()
+);
