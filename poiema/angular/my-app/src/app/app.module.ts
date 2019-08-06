a99e94be6f83b3e91a83ab1c953211c240a88556
abelko <esung1129@gmail.com>
2019-08-06 11:11:22 +0900

    :books: study angular(component, module, directive)

---
+++ poiema/angular/my-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { TodoModule, TodosModule } from './todos/todos.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HighlightDirective } from './highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TodosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
