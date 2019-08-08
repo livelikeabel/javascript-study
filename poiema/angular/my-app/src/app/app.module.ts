06b7e5444ee07e749144bbac546daead6f4300f5
abelko <esung1129@gmail.com>
2019-08-08 23:25:14 +0900

    :art: setting schematic

--- poiema/angular/my-app/src/app/app.module.ts
+++ poiema/angular/my-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HighlightDirective } from './highlight.directive';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HighlightDirective
+    HighlightDirective,
+    TestComponent
   ],
   imports: [
     BrowserModule,
