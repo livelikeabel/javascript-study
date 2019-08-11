9602473ffc0139724191e3ee3a8009f5d18796bd
Abel <esung1129@gmail.com>
2019-08-11 14:30:56 +0900

    :art: apply IoC in injector

--- poiema/angular/my-app/src/app/app.component.ts
+++ poiema/angular/my-app/src/app/app.component.ts
@@ -7,16 +7,13 @@ import { GreetingService } from "./greeting.service";
     <button (click)="sayHi()">Say Hi</button>
     <p>{{ greeting }}</p>
   `,
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
+  providers: [{ provide: GreetingService, useClass: GreetingService }]
 })
 export class AppComponent {
   greeting: string;
-  greetingService: GreetingService;
 
-  constructor() {
-    // 서비스의 인스턴스를 직접 생성한다.
-    this.greetingService = new GreetingService();
-  }
+  constructor(private greetingService: GreetingService) {}
 
   sayHi() {
     // 서비스의 사용
