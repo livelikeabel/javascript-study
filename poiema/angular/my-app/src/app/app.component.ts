b02e5d936542fba8672a4c8cd72d77123c13e83b
Abel <esung1129@gmail.com>
2019-08-11 12:45:09 +0900

    :art: add service

--- poiema/angular/my-app/src/app/app.component.ts
+++ poiema/angular/my-app/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { GreetingService } from "./greeting.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  template: `
+    <button (click)="sayHi()">Say Hi</button>
+    <p>{{ greeting }}</p>
+  `,
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'Hello angular';
+  greeting: string;
+  greetingService: GreetingService;
+
+  constructor() {
+    // 서비스의 인스턴스를 직접 생성한다.
+    this.greetingService = new GreetingService();
+  }
+
+  sayHi() {
+    // 서비스의 사용
+    this.greeting = this.greetingService.sayHi();
+  }
 }
