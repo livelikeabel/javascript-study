09830f680fd6597e2bf2e751a18a1039c1ffa107
abelko <esung1129@gmail.com>
2019-08-15 00:46:10 +0900

    make observable

--- Rx-js/quick-start/chapter2/Observable.js
+++ Rx-js/quick-start/chapter2/Observable.js
@@ -2,17 +2,30 @@ const { Observable } = require('rxjs');
 // 첫번째 파리미터는 function subscribe(observer) 함수를 받을 수 있다.
 // numbers$라는 Observable을 만들었다.
 const numbers$ = new Observable(function subscribe(observer) {
-    observer.next(1);
-    observer.next(2);
-    observer.next(3);
+    try {
+        observer.next(1);
+        observer.next(2);
+        throw new Error('error aquired');
+        observer.next(3);
+    } catch (e) {
+        observer.error(e);
+    }
 });
 
 // subscribe함수에 옵저버를 넘겨준다.
-numbers$.subscribe(v => console.log(v));
+numbers$.subscribe({
+    next: v => console.log(v),
+    error: e => console.error(e)
+});
 
 const numbers2$ = Observable.create(function subscribe(observer) {
     observer.next(1);
     observer.next(2);
     observer.next(3);
+    observer.complete();
 });
-numbers2$.subscribe(v => console.log(v));
+numbers2$.subscribe({
+    next: v => console.log(v),
+    error: e => console.error(e),
+    complete: () => console.log('데이터 전달 완료')
+});
