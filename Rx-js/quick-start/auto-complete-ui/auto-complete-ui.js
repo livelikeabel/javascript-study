5ade91fb7569be25f484130c90728c36984aafb5
abelko <esung1129@gmail.com>
2019-08-19 10:11:16 +0900

    :art: apply debounce, filter, distinctUntilChage

--- Rx-js/quick-start/auto-complete-ui/auto-complete-ui.js
+++ Rx-js/quick-start/auto-complete-ui/auto-complete-ui.js
@@ -1,10 +1,13 @@
 import { fromEvent, from } from 'rxjs';
-import { map, mergeAll, mergeMap } from 'rxjs/operators'
+import { map, mergeAll, mergeMap, debounceTime, filter, distinctUntilChanged } from 'rxjs/operators'
 import { ajax } from 'rxjs/ajax';
 
 const user$ = fromEvent(document.getElementById("search"), "keyup")
     .pipe(
+        debounceTime(300),
         map(e => e.target.value),
+        distinctUntilChanged(),
+        filter(query => query.trim().length > 0),
         mergeMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`))
     );
 
