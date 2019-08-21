fb6d1a34406a86db4f3d698efa9fe6bba879ec2c
abelko <esung1129@gmail.com>
2019-08-22 00:12:14 +0900

    :art: add drawLayer func

--- Rx-js/quick-start/auto-complete-ui/auto-complete-ui.js
+++ Rx-js/quick-start/auto-complete-ui/auto-complete-ui.js
@@ -2,6 +2,19 @@ import { fromEvent, from } from 'rxjs';
 import { map, mergeAll, mergeMap, debounceTime, filter, distinctUntilChanged } from 'rxjs/operators'
 import { ajax } from 'rxjs/ajax';
 
+const $layer = document.getElementById("suggestLayer");
+
+function drawLayer(items) {
+    console.log($layer)
+    console.log($layer.innerHTML)
+    $layer.innerHTML = items.map(user => {
+        return `<li class="user">
+        <img src="${user.avatar_url}" width="50px" height="50px" />
+        <p><a href="${user.html_url}" target="_blank">${user.login}</a></p>
+        </li>`
+    }).join("");
+}
+
 const user$ = fromEvent(document.getElementById("search"), "keyup")
     .pipe(
         debounceTime(300),
@@ -11,4 +24,8 @@ const user$ = fromEvent(document.getElementById("search"), "keyup")
         mergeMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`))
     );
 
-user$.subscribe(v => console.log(v));
+user$.subscribe(v => {
+    console.log(v.items)
+    drawLayer(v.items)
+    console.log($layer.innerHTML)
+});
