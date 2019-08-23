2d3fbe7fe901cebfdcbd38f78e78e45f87aae51c
abelko <esung1129@gmail.com>
2019-08-23 22:48:50 +0900

    :art: add loading UI

--- Rx-js/quick-start/auto-complete-ui/auto-complete-ui.js
+++ Rx-js/quick-start/auto-complete-ui/auto-complete-ui.js
@@ -1,12 +1,11 @@
 import { fromEvent, from } from 'rxjs';
-import { map, mergeAll, mergeMap, debounceTime, filter, distinctUntilChanged } from 'rxjs/operators'
+import { tap, map, mergeAll, mergeMap, debounceTime, filter, distinctUntilChanged } from 'rxjs/operators'
 import { ajax } from 'rxjs/ajax';
 
 const $layer = document.getElementById("suggestLayer");
+const $loading = document.getElementById("loading");
 
 function drawLayer(items) {
-    console.log($layer)
-    console.log($layer.innerHTML)
     $layer.innerHTML = items.map(user => {
         return `<li class="user">
         <img src="${user.avatar_url}" width="50px" height="50px" />
@@ -15,17 +14,25 @@ function drawLayer(items) {
     }).join("");
 }
 
+function showLoading() {
+    $loading.style.display = "block";
+}
+
+function hideLoading() {
+    $loading.style.display = "none";
+}
+
 const user$ = fromEvent(document.getElementById("search"), "keyup")
     .pipe(
         debounceTime(300),
         map(e => e.target.value),
         distinctUntilChanged(),
         filter(query => query.trim().length > 0),
-        mergeMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`))
+        tap(showLoading),
+        mergeMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`)),
+        tap(hideLoading)
     );
 
 user$.subscribe(v => {
-    console.log(v.items)
     drawLayer(v.items)
-    console.log($layer.innerHTML)
 });
