e576eda48f171e5798c530cacbff5bc360a0970a
abelko <esung1129@gmail.com>
2019-09-20 23:42:54 +0900

    :art: modify tribonacci

--- algorithm/codewars/tribonacci.js
+++ algorithm/codewars/tribonacci.js
@@ -1,4 +1,4 @@
-const tribonacci = (arr, n) => arr.push(arr.slice(-3).reduce((a, v) => a + v)) !== 10 ? tribonacci(arr, n - 1) : arr
+const tribonacci = (arr, n) => arr.push(arr.slice(-3).reduce((a, v) => a + v)) !== (n + arr.length - 4) ? tribonacci(arr, n - 1) : arr
 
 console.log(tribonacci([1, 1, 1], 10)) // [1, 1, 1, 3, 5, 9, 17, 31, 57, 105]
 console.log(tribonacci([0, 0, 1], 10)) // [0, 0, 1, 1, 2, 4, 7, 13, 24, 44]
