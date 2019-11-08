2cb563685d351c48be354bf99061b8e4053f83c6
abelko <esung1129@gmail.com>
2019-11-08 09:51:45 +0900

    :art: add drag function

---
+++ Rx-js/quick-start/auto-complete-ui/carousel/index.js
@@ -0,0 +1,36 @@
+import { fromEvent } from 'rxjs';
+import { map, switchMap, takeUntil } from 'rxjs/operators';
+
+const $view = document.getElementById("carousel");
+const $container = $view.querySelector(".container");
+const PANEL_COUNT = $container.querySelectorAll(".panel").length;
+const SUPPORT_TOUCH = "ontouchstart" in window;
+const EVENTS = {
+    start: SUPPORT_TOUCH ? "touchstart" : "mousedown",
+    move: SUPPORT_TOUCH ? "touchmove" : "mousemove",
+    end: SUPPORT_TOUCH ? "touchend" : "mouseup"
+};
+
+function toPos(obs$) {
+    return obs$.pipe(
+        map(v => SUPPORT_TOUCH ? v.changedTouches[0].pageX : v.pageX)
+    );
+}
+
+const start$ = fromEvent($view, EVENTS.start).pipe(toPos);
+const move$ = fromEvent($view, EVENTS.move).pipe(toPos);
+const end$ = fromEvent($view, EVENTS.end);
+
+const drag$ = start$.pipe(
+    switchMap(start => {
+        return move$.pipe(
+            map(move => move - start),
+            takeUntil(end$)
+        );
+    })
+)
+
+// start$.subscribe(e => console.log('start', e))
+// move$.subscribe(e => console.log('move', e))
+// end$.subscribe(e => console.log('end'))
+drag$.subscribe(e => console.log('drag', e))
