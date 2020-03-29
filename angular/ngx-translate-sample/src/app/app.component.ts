4b9748781494d6628327444172df3bb02b6c8aa9
abelko <esung1129@gmail.com>
2020-03-29 15:46:18 +0900

    Add en.json and apply translate

--- angular/ngx-translate-sample/src/app/app.component.ts
+++ angular/ngx-translate-sample/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngx-translate-sample';
+  private translate: TranslateService;
+
+  constructor(translate: TranslateService) {
+    this.translate = translate;
+  }
+
+  public ngOnInit(): void {
+    this.translate.setDefaultLang('en');
+  }
 }
