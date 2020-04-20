0f2c923425021b77266f49ffb8c1e386ac2c2659
abelko <esung1129@gmail.com>
2020-04-21 00:47:31 +0900

    Add prime ver2

--- spitz/lecture-81/prime.js
+++ spitz/lecture-81/prime.js
@@ -10,3 +10,23 @@ const prime1 =(end)=>{
     }
     return prime.concat(num);
 };
+
+const prime2 =(end)=>{
+    const prime = [2], num = [];
+    let curr = 2, i = 3, j;
+    while(i <= end){
+        num.push(i);
+        i += 2;
+    }
+    while(num.length){
+        prime.push(curr = num.shift());
+        i = num.length;
+        j = curr * curr;
+        while(i--){
+            if(num[i] < j) break;
+            if(num[i] % curr == 0) num.splice(i, 1);
+        }
+        if(curr*curr > num[num.length - 1]) break;
+    }
+    return prime.concat(num);
+};
