edb05b515a354c10bfbf7887c60ff34ae7380955
abelko <esung1129@gmail.com>
2020-04-21 09:41:42 +0900

    Add prime ver 4

--- spitz/lecture-81/prime.js
+++ spitz/lecture-81/prime.js
@@ -44,3 +44,18 @@ const prime3 =end=>{
     }
     return num;
 };
+
+const prime4 =end=>{
+    const num = [2];
+    for(let i = 3; i <= end; i += 2) num.push(i);
+    for(let i = 1, j = num.length, curr; i < j; i++){
+        if(curr = num[i]){
+            const min = curr * curr;
+            if(end < min) break;
+            for(let k = (min - 1) / 2; k < j; k += curr){
+                if(num[k] % curr == 0) num[k] = 0;
+            }
+        }
+    }
+    return num.filter(v=>v);
+};
