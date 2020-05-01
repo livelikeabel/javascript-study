2e0d6bd1f3d6fc2c011c98e2bb858c9e7caad72b
abelko <esung1129@gmail.com>
2020-05-02 00:02:51 +0900

    study generator with promise

--- spitz/lecture-85(2)/generator.js
+++ spitz/lecture-85(2)/generator.js
@@ -27,3 +27,24 @@ const ngFor = (max, load, block)=>{
 };
 
 ngFor(100, 10, ()=>console.log('hi'));
+
+const gene2 = function*(max, load, block) {
+  let i = 0;
+  while (i < max) {
+    yield new Promise(res => {
+      let curr = load;
+      while (curr-- && i < max){
+        block();
+        i++;
+      }
+      console.log(i);
+      timeout(res, 0);
+    });
+  }
+};
+
+const nbFor2 = (max, load, block)=>{
+  const iterator = gene2(max, load, block);
+  const next = ({value, done})=> done||value.then(v=>next(iterator.next()));
+  next(iterator.next());
+};
