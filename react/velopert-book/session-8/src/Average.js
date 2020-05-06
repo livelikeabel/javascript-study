6e9e733efad79d4c5f8a82197b493ca12e29390d
abelko <esung1129@gmail.com>
2020-05-07 01:32:55 +0900

    Study useRef hook

--- react/velopert-book/session-8/src/Average.js
+++ react/velopert-book/session-8/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback, useRef } from 'react';
 
 const getAverage = numbers => {
   console.log('calculate average...');
@@ -10,21 +10,23 @@ const getAverage = numbers => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState('');
+  const inputEl = useRef(null);
 
-  const onChange = useCallback( e => {
+  const onChange = useCallback(e => {
     setNumber(e.target.value);
   }, []); // Create function when component render first time
   const onInsert = useCallback(() => {
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber('');
+    inputEl.current.focus();
   }, [number, list]); // Create function when only change number or list
 
   const avg = useMemo(() => getAverage(list), [list]);
 
   return (
     <>
-      <input value={number} onChange={onChange}/>
+      <input value={number} onChange={onChange} ref={inputEl}/>
       <button onClick={onInsert}>Register</button>
       <ul>
         {list.map((value, index) => (
