9097b7f5de6ab86ce521de8556742e9c5552133e
abelko <esung1129@gmail.com>
2020-05-07 21:47:09 +0900

    Init lession 9. Styled component

---
+++ react/velopert-book/session-9/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StyledComponent from "./StyledComponent";
+
+function App() {
+  return (
+    <div>
+      <StyledComponent/>
+    </div>
+  );
+}
+
+export default App;
