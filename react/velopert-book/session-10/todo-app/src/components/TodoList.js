a8e3ce0a0fc51bc4de1604163310e4b6f17a3e26
abelko <esung1129@gmail.com>
2020-05-09 23:56:25 +0900

    react-virtualized를 사용한 렌더링 성능 최적화

--- react/velopert-book/session-10/todo-app/src/components/TodoList.js
+++ react/velopert-book/session-10/todo-app/src/components/TodoList.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from "./TodoListItem";
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        />
+      );
+    },
+    [onRemove, onToggle, todos]
+  );
   return (
     <div className="TodoList">
-      {todos.map(todo => (
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
-      ))}
+      <List
+        className="TodoList"
+        width={512}
+        height={513}
+        rowCount={todos.length}
+        rowHeight={57}
+        rowRenderer={rowRenderer}
+        list={todos}
+        style={{ outline: 'none' }}
+      />
     </div>
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
