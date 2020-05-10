81bdb7a60e7ce86589d27faf9be8705b7c720902
abelko <esung1129@gmail.com>
2020-05-10 12:38:46 +0900

    Study react-router project init

---
+++ react/velopert-book/session-13/router-tutorial/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This project is sample project that practice react-router base</p>
+    </div>
+  );
+};
+
+export default About;
