6622bf9dfac758aff44300041cbbdc831cf20690
abelko <esung1129@gmail.com>
2020-05-11 13:54:00 +0900

    Add react-router

--- react/velopert-book/session-14/news-viewer/src/App.js
+++ react/velopert-book/session-14/news-viewer/src/App.js
@@ -1,16 +1,10 @@
-import React, { useCallback, useState } from 'react';
-import NewsList from "./components/NewsList";
-import Categories from "./components/Categories";
+import React from 'react';
+import { Route } from "react-router-dom";
+import NewsPage from "./pages/NewsPage";
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);
-
   return (
-    <>
-      <Categories category={category} onSelect={onSelect}/>
-      <NewsList category={category}/>
-    </>
+    <Route path="/:category?" component={NewsPage}/>
   );
 };
 
