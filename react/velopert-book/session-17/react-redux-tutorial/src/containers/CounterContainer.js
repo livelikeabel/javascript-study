db0f3626ddcd36b0de0dc7a37a564c021aaccfac
abelko <esung1129@gmail.com>
2020-05-13 09:29:19 +0900

    Add CounterContainer and connect with redux

---
+++ react/velopert-book/session-17/react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Counter from "../components/Counter";
+import { connect } from "react-redux";
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (<Counter number={number} onIncrease={increase} onDecrease={decrease}/>);
+};
+
+const mapStateToProps = state => ({
+  number: state.counter.number
+});
+const mapDispatchToProps = dispatch => ({
+  increase: () => {
+    dispatch(increase());
+  },
+  decrease: () => {
+    dispatch(decrease());
+  }
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CounterContainer);
--- react/velopert-book/session-17/react-redux-tutorial/src/index.js
+++ react/velopert-book/session-17/react-redux-tutorial/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store =createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
