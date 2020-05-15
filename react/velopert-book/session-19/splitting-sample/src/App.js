740582f08bac258fa74708d3d5240b475e1ece0f
abelko <esung1129@gmail.com>
2020-05-15 10:23:24 +0900

    Study Loadable

--- react/velopert-book/session-19/splitting-sample/src/App.js
+++ react/velopert-book/session-19/splitting-sample/src/App.js
@@ -1,17 +1,22 @@
 import React, { useState, Suspense } from 'react';
 import './App.css';
-
-const SplitMe = React.lazy(() => import('./SplitMe'));
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
 
 const App = () => {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <p onClick={onClick}>Hello React</p>
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React</p>
         <Suspense fallback={<div>Loading...</div>}>
           {visible && <SplitMe/>}
         </Suspense>
