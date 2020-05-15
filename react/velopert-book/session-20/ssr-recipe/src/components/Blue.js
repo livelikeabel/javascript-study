32c2c3fd502847adb426008ff431308d8e914cf2
abelko <esung1129@gmail.com>
2020-05-15 10:42:24 +0900

    Init server side render project

---
+++ react/velopert-book/session-20/ssr-recipe/src/components/Blue.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Blue.css';
+
+const Blue = () => {
+  return (
+    <div className="Blue">
+      Blue
+    </div>
+  );
+};
+
+export default Blue;
---
+++ react/velopert-book/session-20/ssr-recipe/src/components/Red.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Red.css';
+
+const Red = () => {
+  return (
+    <div className="Red">
+      Red
+    </div>
+  );
+};
+
+export default Red;
