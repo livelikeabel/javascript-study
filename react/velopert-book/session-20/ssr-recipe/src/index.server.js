a466a0c41a23870c9c5ed84ca10120379bdb79d9
Abel <esung1129@gmail.com>
2020-05-29 01:15:21 +0900

    Serve static file

--- react/velopert-book/session-20/ssr-recipe/src/index.server.js
+++ react/velopert-book/session-20/ssr-recipe/src/index.server.js
@@ -3,6 +3,45 @@ import ReactDOMServer from "react-dom/server";
 import express from "express";
 import { StaticRouter } from "react-router-dom";
 import App from "./App";
+import path from "path";
+import fs from "fs";
+
+// search file paths at asset-manifest.json
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve("./build/asset-manifest.json"), "utf8")
+);
+
+const chunks = Object.keys(manifest.files)
+  .filter(key => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾아서
+  .map(key => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
+  .join(""); // 합침
+
+function createPage(root) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale-1,shrink-to-fit=no"
+    />
+    <meta name="theme-color" content="#000000" />
+    <title>React App</title>
+    <link href="${manifest.files["main.css"]}" rel="stylesheet" />
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root">
+      ${root}
+    </div>
+    <script src="${manifest.files["runtime-main.js"]}"></script>
+    ${chunks}
+    <script src="${manifest.files["main.js"]}"></script>
+  </body>
+  </html>
+  `;
+}
 
 const app = express();
 
@@ -17,9 +56,14 @@ const serverRender = (req, res, next) => {
     </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-  res.send(root); // 클라이언트에게 결과물을 응답한다.
+  res.send(createPage(root)); // 클라이언트에게 결과물을 응답한다.
 };
 
+const serve = express.static(path.resolve("./build"), {
+  index: false // "/" 경로에서 index.html을 보여 주지 않도록 설정
+});
+
+app.use(serve); // 순서가 중요. serverRender 전에 위치해야 한다.
 app.use(serverRender);
 
 // Listen server 5000 port
