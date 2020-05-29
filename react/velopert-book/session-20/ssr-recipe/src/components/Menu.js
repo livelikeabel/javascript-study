549c7c0b78d406497f53d21e49e6fe53c2a9051b
Abel <esung1129@gmail.com>
2020-05-29 19:38:45 +0900

    Add Users, UsersContainer component

--- react/velopert-book/session-20/ssr-recipe/src/components/Menu.js
+++ react/velopert-book/session-20/ssr-recipe/src/components/Menu.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 const Menu = () => {
   return (
     <ul>
@@ -8,6 +8,9 @@ const Menu = () => {
       </li>
       <li>
         <Link to="/blue">Blue</Link>
+      </li>
+      <li>
+        <Link to="/users">Users</Link>
       </li>
     </ul>
   );
