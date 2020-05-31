7b5d80f7e3322d196f0ddd5cdae183ad72bcd541
Abel <esung1129@gmail.com>
2020-05-31 21:21:15 +0900

    Prepare redux-saga code

--- react/velopert-book/session-20/ssr-recipe/src/index.js
+++ react/velopert-book/session-20/ssr-recipe/src/index.js
@@ -7,13 +7,18 @@ import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import rootReducer from "./modules";
+import createSagaMiddleware from "redux-saga";
+import rootReducer, { rootSaga } from "./modules";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   window.__PRELOADED_STATE__, // 이 값을 초기 상태로 사용한다.
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, sagaMiddleware)
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <React.StrictMode>
