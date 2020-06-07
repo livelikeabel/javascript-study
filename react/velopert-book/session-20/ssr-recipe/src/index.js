9a8da0b178ea47e2c6c09c08413e53a76a68a126
Abel ko <abel.ko@rainist.com>
2020-06-08 06:16:02 +0900

    Add loadableReady and hydrate

--- react/velopert-book/session-20/ssr-recipe/src/index.js
+++ react/velopert-book/session-20/ssr-recipe/src/index.js
@@ -9,6 +9,7 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
 import rootReducer, { rootSaga } from "./modules";
+import { loadableReady } from '@loadable/component';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -20,16 +21,28 @@ const store = createStore(
 
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(
-  <React.StrictMode>
+// 같은 내용을 쉽게 재사용할 수 있도록 렌더링할 내용을 하나의 컴포넌트로 묶음
+const Root = () => {
+  return (
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  );
+};
+
+const root = document.getElementById("root");
+
+// 프로덕션 환경에서는 loadableReady와 hydrate를 사용하고
+// 개발 환경에서는 기존 방식으로 처리
+if (process.env.NODE_ENV === 'production') {
+  loadableReady(() => {
+    ReactDOM.hydrate(<Root />, root);
+  });
+} else {
+  ReactDOM.render(<Root />, root);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
